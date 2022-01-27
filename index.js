11dc25298ea9df9be8b9914bde58f99e65f87c9a
samuaks <samusoisalo@hotmail.com>
2022-01-27 14:32:56 +0200

    added sockets and root response serves html page

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server); 
 const lights = require('./services/lights');
 const wifi = require('./services/wifi');
 const scan = require('./services/devices');
 
-const app = express();
 const port = 3000;
 
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/pages/index.html'));
 app.get('/lights', lights.getAllLights);
 app.get('/lights/:id', lights.getLight);
 app.put('/lights/:id', lights.turnOfforOnLight);
@@ -15,8 +19,25 @@ app.put('/lights', lights.turnOfforOnAllLights);
 app.get('/wifi', wifi.listWifi);
 
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('turn on', (msg) => {
+        io.emit('turn on', msg);
+        lights.turnOnAllLights();
+
+    })
+    socket.on('turn off', (msg) => {
+        io.emit('turn off', msg);
+        lights.turnOffAllLights();
+        });
+});
+
+
+server.listen(port, () => {
     console.log('Listen on port ' + port);
-    scan.deviceScanning();
-    setInterval(scan.deviceScanning, 5000);
+    /* scan.deviceScanning(); */
+    /* setInterval(scan.deviceScanning, 5000); */
 })
